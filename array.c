bfce962ddd93fda37afd5f8b329c5ebe7581bb98
Daniel Zhou <zhoud3713@my.sduhsd.net>
2020-01-08 13:02:31 -0800

    added array.c

---
+++ array.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void arrayAdd(int n, int s, int array[])
+{
+  for (int i = 0; i < s; i++)
+  {
+    array[i] = array[i] + n;
+    printf("%d\n", array[i]);
+  }
+}
+
+int main()
+{
+  // create an int array 5 long
+  int arr[5];
+  
+  for (int i = 0; i < 5; i++)
+  {
+    arr[i] = i * i;
+  }
+
+  printf("1st and 3rd vals: %d, %d\n", arr[0], arr[2]);
+  
+  printf("Or %d, %d\n", *arr, *(arr+2));
+
+  
+  // create array to hold intialized values
+  int arr2[] = { 1, 2, 3, 4 };
+  for (int i = 0; i < 4; i++)
+  {
+    printf("%d, ", arr2[i]);
+  }
+  printf("\n");
+
+  int array[100];
+
+  for (int i = 0; i < 101; i++)
+  {
+    array[i] = i * i;
+  }
+
+  arrayAdd(53, 100, array);
+}
