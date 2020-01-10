87ebb2d9987fa9a4ee7085c41435fac44050956c
Daniel Zhou <daniel.s.zhou@gmail.com>
2020-01-10 08:00:18 -0800

    added student.c

--- student.c
+++ student.c
@@ -1,23 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct Student {
-  char firstName[50];
-  char lastName[50];
-  int age[5];
-  int studentID[30];
+  char firstName[30];
+  char lastName[30];
+  int age;
+  int studentID;
 };
 
-void printStudent(struct Student* student);
+void printStudent(struct Student* students) {
+  printf("Student name: %s %s, Age: %d, Student ID: %d\r\n", students->firstName, students->lastName, students->age, students->studentID);
+}
 
 int main() {
-  struct Student Student1;
+  
+  int count = 0;
+  char cont[10];
+  char input[256];
 
-  printStudent( Student1 );
+  struct Student students[20];
+  
+  for ( int i = 0; i < 20; i++ ) {
+    
+    printf("First Name: ");
+    fgets(input, 256, stdin);
+    sscanf(input, "%s", students[i].firstName);
+
+    printf("Last Name: ");
+    fgets(input, 256, stdin);
+    sscanf(input, "%s", students[i].lastName);
+
+    printf("Age: ");
+    fgets(input, 256, stdin);
+    sscanf(input, "%d", &students[i].age);
+
+    printf("Student ID: ");
+    fgets(input, 256, stdin);
+    sscanf(input, "%d", &students[i].studentID);
+
+    printf("%s, %s, %d, %d\n", students[i].firstName, students[i].lastName, students[i].age, students[i].studentID);
+
+    count++;
+
+    printf("Do you wish to continue? Press n to stop.");
+    fgets(input, 10, stdin);
+    if ( sscanf(input, "%s", cont) == 1) {
+      if ( strcmp(cont, "n") == 0) {
+	  break;
+        }       
+    }
+  }
+
+  for (int r = 0; r < count; r++ ) {
+    printStudent( &students[r] );
+  }
 }
 
-void printStudent(struct Student* student) {
-  printf("first name is:%s\n", student->firstName);
-  printf("last name is:%s\n", student->lastName);
-  printf("age:%s\n", student->age);
-  printf("student ID:%s\n",student->studentID);
-}
